b525265ac0e804fb217af8b4dd0b5e2ef03e2838
Marco Guilherme <marcoguilhermepereira@outlook.com>
2025-10-16 22:57:14 -0300

    Projeto Spring inicial gerado pelo Spring Initializr. https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.6&packaging=jar&jvmVersion=25&groupId=br.com&artifactId=hydromarket&name=HydroMarket&description=Backend%20do%20programa%20de%20gerenciamento%20de%20vendas%20dos%20produtos%20hidrop%C3%B4nicos.&packageName=br.com.hydromarket&dependencies=

---
+++ src/main/java/br/com/hydromarket/HydroMarketApplication.java
@@ -0,0 +1,11 @@
+package br.com.hydromarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroMarketApplication {
+	public static void main(String[] arguments) {
+		SpringApplication.run(HydroMarketApplication.class, arguments);
+	}
+}
---
+++ src/test/java/br/com/hydromarket/HydroMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hydromarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydroMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
